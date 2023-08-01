c49c7d50aa7b4e2a56140987d6613f963bb888ff
iannacus <ian.rosas@academlo.com>
2023-07-31 19:54:04 -0600

    production

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 module.exports = {
   development: {
     username: "iannacus",
@@ -6,7 +7,7 @@ module.exports = {
     port: 5432,
     host: "localhost",
     dialect: "postgres",
-    logging: false,
+    logging: false, // no uestra en consola los console.log de sequelize
   },
   test: {
     username: "root",
@@ -16,11 +17,12 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    dialectOptions: {},
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    dialectOptions: { ssl: { required: true, rejectUnauthorized: false } },
   },
 };
